620d189de39f2d92f580746ddcf3b31bd5fa2321
Kresha Shah <shahkresha953@gmail.com>
2026-01-11 21:22:26 +0530

    editing

--- MY_Project/settings.py
+++ MY_Project/settings.py
@@ -89,10 +89,11 @@ WSGI_APPLICATION = 'MY_Project.wsgi.application'
 #  https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ["DATABASE_URL"]
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
     )
- }
+}
+
 
 # DATABASES = {
 #     'default': dj_database_url.config(
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import WasteForm
 from .models import Waste
+from .ml_pricing import predict_price
 
-# Create your views here.
+
+# Home page – latest listings
 def home(request):
     latest_wastes = Waste.objects.filter(
         status='available'
@@ -13,26 +15,55 @@ def home(request):
         request,
         'index.html',
         {'latest_wastes': latest_wastes}
-)
+    )
 
+
+# Upload waste + ML price prediction
 @login_required
 def upload_waste(request):
     if request.method == 'POST':
         form = WasteForm(request.POST, request.FILES)
+
         if form.is_valid():
             waste = form.save(commit=False)
             waste.user = request.user
+
+            # 🔥 ML PRICE PREDICTION
+            waste.predicted_price = predict_price(
+                waste.waste_type,
+                waste.weight_kg
+            )
+
+            # Initially final price = predicted price
+            waste.final_price = waste.predicted_price
+
             waste.save()
             return redirect('home')
+
     else:
         form = WasteForm()
 
-    return render(request, 'marketplace/upload_waste.html', {'form': form})
+    return render(
+        request,
+        'marketplace/upload_waste.html',
+        {'form': form}
+    )
 
+
+# Marketplace page
 def marketplace(request):
-    wastes = Waste.objects.filter(status='available').order_by('-created_at')
-    return render(request, 'marketplace/marketplace.html', {'wastes': wastes})
+    wastes = Waste.objects.filter(
+        status='available'
+    ).order_by('-created_at')
 
+    return render(
+        request,
+        'marketplace/marketplace.html',
+        {'wastes': wastes}
+    )
+
+
+# User's own listings
 @login_required
 def my_listings(request):
     wastes = Waste.objects.filter(
