acbbf74075580c3d492da9da8be189bf36d6aab1
Kresha Shah <shahkresha953@gmail.com>
2026-01-12 21:29:30 +0530

    Fix ML pricing and Cloudinary integration

--- MY_Project/settings.py
+++ MY_Project/settings.py
@@ -169,11 +169,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 if os.environ.get("CLOUDINARY_CLOUD_NAME"):
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-    CLOUDINARY_STORAGE = {
-        'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-        'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-        'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
-    }
+    # CLOUDINARY_STORAGE = {
+    #     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    #     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    #     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+    # }
 else:
     # Local fallback
     DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
