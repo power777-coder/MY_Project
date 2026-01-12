4c6bf08641da39cb1782e165097b03ec71495f53
Kresha Shah <shahkresha953@gmail.com>
2026-01-12 21:06:04 +0530

    still updating images

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,15 +1,27 @@
 from django.contrib import admin
 from .models import Waste
+from django.utils.html import format_html
+
 
 @admin.register(Waste)
 class WasteAdmin(admin.ModelAdmin):
     list_display = (
         'id',
         'waste_type',
-        'user',
         'weight_kg',
-        'status',
-        'created_at',
+        'final_price',
+        'image_preview',
+        'created_at'
     )
-    list_filter = ('waste_type', 'status')
-    search_fields = ('user__username', 'description')
+
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" width="150" style="border-radius:8px;" />',
+                obj.image.url
+            )
+        return "No Image"
+
+    image_preview.short_description = "Image Preview"
