985017e6ffed6a1cf988e854423d1ec39ddeb07f
Kresha Shah <shahkresha953@gmail.com>
2026-01-15 18:28:04 +0530

    just solving

--- MY_Project/settings.py
+++ MY_Project/settings.py
@@ -197,5 +197,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_USER = 'waste.2wealth0@gmail.com'
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
